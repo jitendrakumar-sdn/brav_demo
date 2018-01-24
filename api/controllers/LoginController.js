35bde5e89c9e16f18181c88358802aef132df2ee
Thomsheer Ahamedm <ahmedtamseer@gmail.com>
2018-01-24 20:48:03 +0530

    Completed chat implementation But UI is remaining for some video and chat UI we will get it done by tomorrow

---
+++ api/controllers/LoginController.js
@@ -0,0 +1,16 @@
+/**
+ * LoginController
+ *
+ * @description :: Server-side logic for managing Logins
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function(req, res) {
+		res.render('pages/login');
+	},
+
+	checkLogin: function(req, res) {
+		
+	}
+};
---
+++ api/controllers/MediatorController.js
@@ -0,0 +1,12 @@
+/**
+ * MediatorController
+ *
+ * @description :: Server-side logic for managing mediators
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	index: function(req, res) {
+		res.render('pages/mediator', {_layoutFile: '../shared/mediator_layout.ejs', id: req.param('id')});
+	}
+};
