def8950a2c4f18cbe764fefcfba04d43903fde13
Sanchay Shukla <sanchay.shukla@smartdatainc.net>
2018-01-31 14:29:34 +0530

    Updated key files

--- config/env/development.js
+++ config/env/development.js
@@ -11,6 +11,7 @@
  */
 
 module.exports = {
+  port: 5102
 
   /***************************************************************************
    * Set the default database connection for models in the development       *
